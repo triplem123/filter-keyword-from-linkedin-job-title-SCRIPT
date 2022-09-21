3188f79ef58d76dec301fd98d3f8aa8e0db72d23
triplem123 <triplem804@gmail.com>
2022-09-20 23:56:12 -0400

    finished

---
+++ job_filter.js
@@ -0,0 +1,18 @@
+// filter out senior jobs from linkedin
+javascript:
+var i = 0;
+var x = document.getElementsByClassName('jobs-search-results-list')[0];
+function filter() {
+    document.querySelectorAll("li").forEach(function (e) {
+        e.id && e.innerHTML.match(/Senior/) && e.remove()
+    }),
+        setTimeout(function () {
+            x.scrollTop = 0
+        }, 0)
+}
+!function e() {
+    x.scroll(0, 100 * ++i), i > 40 ? filter() : setTimeout(e, 0)
+}();
+
+// javascript:
+// var i = 0; var x = document.getElementsByClassName('jobs-search-results-list')[0]; function filter() {document.querySelectorAll("li").forEach(function (e) {e.id && e.innerHTML.match(/Senior/) && e.remove()}),setTimeout(function () {x.scrollTop = 0}, 0)} !function e() {x.scroll(0, 100 * ++i), i > 40 ? filter() : setTimeout(e, 0)}();
