b4326b1278022ed70f9d6575337ed972e794dbe6
triplem123 <90787133+triplem123@users.noreply.github.com>
2022-09-23 16:59:19 -0400

    edits

--- job_filter.js
+++ job_filter.js
@@ -2,17 +2,22 @@
 javascript:
 var i = 0;
 var x = document.getElementsByClassName('jobs-search-results-list')[0];
-function filter() {
+var arr = [/Senior/, /Staff/, /Lead/, /Principal/];
+function filter(word) {
     document.querySelectorAll("li").forEach(function (e) {
-        e.id && e.innerHTML.match(/Senior/) && e.remove()
+        e.id && e.innerHTML.match(word) && e.remove()
     }),
         setTimeout(function () {
             x.scrollTop = 0;
         }, 0)
 }
 !function e() {
-    x.scroll(0, 100 * ++i), i > 40 ? filter() : setTimeout(e, 0)
+    x.scroll(0, 100 * ++i), i > 40 ? filter(arr.pop()) : setTimeout(e, 0)
 }();
+
+
 
 // javascript:
 // var i = 0; var x = document.getElementsByClassName('jobs-search-results-list')[0]; function filter() {document.querySelectorAll("li").forEach(function (e) {e.id && e.innerHTML.match(/Senior/) && e.remove()}),setTimeout(function () {x.scrollTop = 0}, 0)} !function e() {x.scroll(0, 100 * ++i), i > 40 ? filter() : setTimeout(e, 0)}();
+
+// javascript: var i = 0; var x = document.getElementsByClassName('jobs-search-results-list')[0]; var arr = [/Senior/, /Staff/, /Lead/, /Principal/]; function filter(word) { document.querySelectorAll("li").forEach(function (e) { e.id && e.innerHTML.match(word) && e.remove() }), setTimeout(function () { x.scrollTop = 0; }, 0) } !function e() { x.scroll(0, 100 * ++i), i > 40 ? filter(arr.pop()) : setTimeout(e, 0) }();
