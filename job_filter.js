7b564db081b02ac7c93497af20f1fa797066f288
triplem123 <triplem804@gmail.com>
2022-09-23 20:45:54 -0400

    Finished script for Senior, Staff, Lead, and Principal positions

--- job_filter.js
+++ job_filter.js
@@ -3,21 +3,20 @@ javascript:
 var i = 0;
 var x = document.getElementsByClassName('jobs-search-results-list')[0];
 var arr = [/Senior/, /Staff/, /Lead/, /Principal/];
-function filter(word) {
-    document.querySelectorAll("li").forEach(function (e) {
-        e.id && e.innerHTML.match(word) && e.remove()
-    }),
-        setTimeout(function () {
-            x.scrollTop = 0;
-        }, 0)
-}
-!function e() {
-    x.scroll(0, 100 * ++i), i > 40 ? filter(arr.pop()) : setTimeout(e, 0)
-}();
-
-
-
-// javascript:
-// var i = 0; var x = document.getElementsByClassName('jobs-search-results-list')[0]; function filter() {document.querySelectorAll("li").forEach(function (e) {e.id && e.innerHTML.match(/Senior/) && e.remove()}),setTimeout(function () {x.scrollTop = 0}, 0)} !function e() {x.scroll(0, 100 * ++i), i > 40 ? filter() : setTimeout(e, 0)}();
+function filter(word) { 
+    document.querySelectorAll("li").forEach(function (e) { 
+        e.id && e.innerHTML.match(word) && e.remove() 
+    }), setTimeout(function () { 
+        x.scrollTop = 0; 
+    }, 0); 
+    x.scroll(0, 0); 
+    e(); 
+} 
+function e() { 
+    x.scroll(0, 100 * ++i); 
+    if (i > 160) { return; } 
+    i % 40 === 0 ? filter(arr.pop()) : setTimeout(e, 0) 
+}; 
+e();
 
 // javascript: var i = 0; var x = document.getElementsByClassName('jobs-search-results-list')[0]; var arr = [/Senior/, /Staff/, /Lead/, /Principal/]; function filter(word) { document.querySelectorAll("li").forEach(function (e) { e.id && e.innerHTML.match(word) && e.remove() }), setTimeout(function () { x.scrollTop = 0; }, 0) } !function e() { x.scroll(0, 100 * ++i), i > 40 ? filter(arr.pop()) : setTimeout(e, 0) }();
