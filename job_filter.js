1acad8a85523d41875f53f817b436151c3f749b0
triplem123 <90787133+triplem123@users.noreply.github.com>
2022-09-25 21:36:12 -0400

    update

--- job_filter.js
+++ job_filter.js
@@ -20,4 +20,4 @@ function e() {
 }; 
 e();
 
-// javascript: var i = 0; var x = document.getElementsByClassName('jobs-search-results-list')[0]; var arr = [/Senior/, /Staff/, /Lead/, /Principal/]; function filter(word) { document.querySelectorAll("li").forEach(function (e) { e.id && e.innerHTML.match(word) && e.remove() }), setTimeout(function () { x.scrollTop = 0; }, 0) } !function e() { x.scroll(0, 100 * ++i), i > 40 ? filter(arr.pop()) : setTimeout(e, 0) }();
+// javascript: var i = 0; var x = document.getElementsByClassName('jobs-search-results-list')[0]; var arr = [/Intern/, /Senior/, /Staff/, /Lead/, /Principal/]; var len = arr.length; function filter(word) { document.querySelectorAll("li").forEach(function (e) { e.id && e.innerHTML.match(word) && e.remove() }), setTimeout(function () { x.scrollTop = 0; }, 0); x.scroll(0, 0); e() } function e() { x.scroll(0, 100 * ++i); if (i > (len * 40)) { return; } i % 40 === 0 ? filter(arr.pop()) : setTimeout(e, 0) }; e();
